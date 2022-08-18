b22a0d7aa0d4e2b5cf2d6f88930ad52e59d9c8d7
qadir_dev <qadir.qanbarpoor@gmail.com>
2022-08-18 17:40:54 +0430

    3,15

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
 
-const App = () => {
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      name : 'Qadir'
+    }
+  };
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey Sexy Boy</p>
+        <p>Hey {this.state.name}</p>
       </header>
     </div>
-  );
+  )
+  }
 };
 
 export default App;
